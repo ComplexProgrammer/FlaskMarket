2bf927f0189d1bae3de13422cbb24719234d00dc
odilj <gaybullayevodiljon@gmail.com>
2022-02-06 20:15:00 +0500

    Initial commit

---
+++ market/__init__.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
+app.config['SECRET_KEY'] = '7df06660a1e6b95c9108cdea'
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = "login_page"
+login_manager.login_message_category = "info"
+from market import routes
